8a9937f4241b82963251fb8b4899c622b338654b
Devin Mullins <twifkak@google.com>
2022-10-20 23:22:44 -0700

    SXG in Cache

--- distributor/sw.js
+++ distributor/sw.js
@@ -3,6 +3,14 @@ self.addEventListener('fetch', event => {
   let url = new URL(request.url);
   let {pathname} = url;
   if (pathname.startsWith('/doc/') || pathname.startsWith('/crt/') || pathname.startsWith('/sub/')) {
-    event.respondWith(fetch(request));
+    event.respondWith((async () => {
+      let cache = await caches.open('sxg');
+      let response = await cache.match(request);
+      if (!response) {
+        response = await fetch(request);
+        await cache.put(request, response);
+      }
+      return response;
+    })());
   }
 });
